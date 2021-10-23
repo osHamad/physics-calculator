e54c56290ca9a03f7376a37fa3f050aee1d33c50
Osama Hamad <osamahama2005@gmail.com>
2021-10-23 15:08:12 -0400

    add route usage

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log('hello wrold')
+const express = require('express')
+const app = express()
+const unitRouter = require('./routes/unit_selection')
+
+app.use("/static", express.static('./static/'))
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname+'/views/index.html')
+})
+
+app.use('/units', unitRouter)
+
+app.listen(8000)

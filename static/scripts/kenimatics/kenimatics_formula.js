546b6cb89b1d6a155dfd2568f3a6f895a62b4551
Osama Hamad <osamahama2005@gmail.com>
2021-10-25 20:49:12 -0400

    add calculation button function

--- static/scripts/kenimatics/kenimatics_formula.js
+++ static/scripts/kenimatics/kenimatics_formula.js
@@ -6,7 +6,7 @@ function dGivenV1TA(v1, t, a){
 }
 
 // displacement given final velocity, time and acceleration
-function dGivenV1TA(v2, t, a){
+function dGivenV2TA(v2, t, a){
     return v2 * t - 0.5 * a * t ** 2
 }
 
@@ -17,12 +17,18 @@ function dGivenV1V2T(v1, v2, t){
 
 // FINAL VELOCITY
 // final velocity given initial velocity, acceleration and time
-function v2GivenV1AT(v1, a, t){
+function v2GivenV1TA(v1, t, a){
+    a = parseFloat(a)
+    v1 = parseFloat(v1)
+    t = parseFloat(t)
     return v1 + a * t
 }
 
 // final velocity given initial velocity, acceleration and displacement
 function v2GivenV1AD(v1, a, d){
+    a = parseFloat(a)
+    v1 = parseFloat(v1)
+    d = parseFloat(d)
     return Math.sqrt(v1 ** 2 * a * d)
 }
 
@@ -32,29 +38,39 @@ function aGivenV1V2T(v1, v2, t){
     return (v2 - v1) / t
 }
 
+// function needs optomization
 document.getElementById('calculate-button').addEventListener('click', ()=>{
-    const calculatorVar = [
-    [document.getElementById('velocity1-input'), 'v1'],
-    [document.getElementById('velocity2-input'), 'v2'],
-    [document.getElementById('time-input'), 't'],
-    [document.getElementById('acceleration-input'), 'a'],
-    [document.getElementById('displacement-input'), 'd']
-    ]
-    // store variable we are calculating
-    let mainVar
-    // storing available variables
-    let availableVars = []
-    // check which variable we are calculating
-    for (let i=0; i<Object.keys(calculatorVar).length; i++){
-        if (calculatorVar[i][0] == null){
-            mainVar = calculatorVar[i][1]
-        } else if (calculatorVar[i][0].value != ''){     // check if the inputs are filled in
-            availableVars.push(calculatorVar[i][1])
+    // store all variables
+    const v1 = document.getElementById('velocity1-input')
+    const v2 = document.getElementById('velocity2-input')
+    const t = document.getElementById('time-input')
+    const a = document.getElementById('acceleration-input')
+    const d = document.getElementById('displacement-input')
+
+    let answer
+    
+    // check which function to use
+    // displacement functions
+    if (d == null){
+        // given v1, t and a
+        if (v1.value != '' && t.value != '' && a.value != ''){
+            answer = dGivenV1TA(v1.value, t.value, a.value)
+        }
+        // given v2, t, a
+        else if (v2.value != '' && t.value != '' && a.value != ''){
+            answer = dGivenV2TA(v2.value, t.value, a.value)
+        }
+        // given v1, v2, t
+        else if (v1.value != '' && v2.value != '' && t.value != ''){
+            answer = dGivenV1V2T(v1.value, v2.value, t.value)
+        }
+    } else if (v2 == null){
+        if (v1.value != '' && t.value != '' && a.value != ''){
+            answer = v2GivenV1TA(v1.value, t.value, a.value)
+        } else if (v1.value != '' && a.value != '' && d.value != ''){
+            answer = v2GivenV1AD(v1.value, a.value, d.value)
         }
     }
-    const findFunction = {
-        'v2': {}
-        // make it so that you can find the required function (found above) by seeing the mainVar and the availableVars
-    }
+    
     
 })

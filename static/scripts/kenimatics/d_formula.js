7af846ed9865b6bb0442e5b1835bee7565ce17c7
Osama Hamad <osamahama2005@gmail.com>
2021-10-29 11:04:41 -0400

    add formula images for displacement

--- static/scripts/kenimatics/d_formula.js
+++ static/scripts/kenimatics/d_formula.js
@@ -28,27 +28,31 @@ document.getElementById('calculate-button').addEventListener('click', ()=>{
     const a = document.getElementById('acceleration-input').value
 
     let answer
-    let image
+    let image = document.getElementById('formula-img')
     
     // check which function to use
     // given v1, t, a
     if (v1 != '' && t != '' && a != ''){
-        answer = dGivenV1TA(v1, t, a)
+        answer = dGivenV1TA(v1, t, a) + 'm'
+        image.src = 'https://equatio-api.texthelp.com/svg/%5CDelta%20d%3D%5Cvec%7Bv_1%7D%5CDelta%20t%2B%5Cfrac%7B1%7D%7B2%7D%5Cvec%7Ba%7D%5CDelta%20t%5E2'
     }
     // given v2, t, a
     else if (v2 != '' && t != '' && a != ''){
-        answer = dGivenV2TA(v2, t, a)
+        answer = dGivenV2TA(v2, t, a) + 'm'
+        image.src = 'https://equatio-api.texthelp.com/svg/%5CDelta%20d%3D%5Cvec%7Bv_2%7D%5CDelta%20t-%5Cfrac%7B1%7D%7B2%7D%5Cvec%7Ba%7D%5CDelta%20t%5E2'
     }
     // given v1, v2, t
     else if (v1 != '' && v2 != '' && t != ''){
-        answer = dGivenV1V2T(v1, v2, t)
+        answer = dGivenV1V2T(v1, v2, t) + 'm'
+        image.src = 'https://equatio-api.texthelp.com/svg/%5CDelta%20d%3D%5Cfrac%7B%5Cvec%7Bv_1%7D%2B%5Cvec%7Bv_2%7D%7D%7B2%7D%5CDelta%20t'
     }
     // given v1, v2, a
     else if (v1 != '' && v2 != '' && t != ''){
-        answer = dGivenV1V2A(v1, v2, t)
+        answer = dGivenV1V2A(v1, v2, t) + 'm'
+        image.src = 'https://equatio-api.texthelp.com/svg/%5CDelta%20d%3D%5Cfrac%7B%5Cvec%7Bv_2%7D%5E2-%5Cvec%7Bv_1%7D%5E2%7D%7B2%5Cvec%7Ba%7D%7D'
     }
     else {
         answer = 'There was a problem with your input'
     }
-    document.getElementById('answer').innerText = 'ANSWER: ' + answer + 'm'
+    document.getElementById('answer').innerText = 'ANSWER: ' + answer
 })

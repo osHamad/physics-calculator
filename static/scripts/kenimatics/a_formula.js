40a2b3e7ec471a03e98ffae31c1372e3116ace69
Osama Hamad <osamahama2005@gmail.com>
2021-11-01 10:49:44 -0400

    add formula images for acceleration

--- static/scripts/kenimatics/a_formula.js
+++ static/scripts/kenimatics/a_formula.js
@@ -33,23 +33,23 @@ document.getElementById('calculate-button').addEventListener('click', ()=>{
     // check which function to use
     // given v1, v2, t
     if (v1 != '' && v2 != '' && t != ''){
-        answer = aGivenV1V2T(v1, v2, t) + 's'
+        answer = aGivenV1V2T(v1, v2, t) + 'm/s^2'
         image.src = 'https://equatio-api.texthelp.com/svg/%5Cvec%7Ba%7D%3D%5Cfrac%7Bv_2-v_1%7D%7B%5CDelta%20t%7D'
     }
     // given v1, v2, d
     else if (v1 != '' && v2 != '' && d != ''){
-        answer = aGivenV1V2D(v1, v2, d) + 's'
-        image.src = ''
+        answer = aGivenV1V2D(v1, v2, d) + 'm/s^2'
+        image.src = 'https://equatio-api.texthelp.com/svg/%5Cvec%7Ba%7D%3D%5Cfrac%7B%5Cvec%7Bv_2%7D%5E2-%5Cvec%7Bv_1%7D%5E2%7D%7B2%5CDelta%20d%7D'
     }
     // given v1, d, t
     else if (v1 != '' && d != '' && t != ''){
-        answer = aGivenV1DT(v1, t, d) + 's'
-        image.src = ''
+        answer = aGivenV1DT(v1, t, d) + 'm/s^2'
+        image.src = 'https://equatio-api.texthelp.com/svg/%5Cvec%7Ba%7D%3D%5Cfrac%7B2%5Cleft(%5CDelta%20d-%5Cvec%7Bv_1%7D%5CDelta%20t%5Cright)%7D%7B%5CDelta%20t%5E2%7D'
     }
     // given v2, d, t
     else if (v2 != '' && d != '' && t != ''){
-        answer = aGivenV2DT(v2, t, d) + 's'
-        image.src = ''
+        answer = aGivenV2DT(v2, t, d) + 'm/s^2'
+        image.src = 'https://equatio-api.texthelp.com/svg/%5Cvec%7Ba%7D%3D%5Cfrac%7B-2%5Cleft(%5CDelta%20d-%5Cvec%7Bv_2%7D%5CDelta%20t%5Cright)%7D%7B%5CDelta%20t%5E2%7D'
     }
     else {
         answer = 'There was a problem with your input'

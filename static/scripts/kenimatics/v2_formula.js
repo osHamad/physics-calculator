cfae532f19403ee2dd2523c9d173ab9d2d28362a
Osama Hamad <osamahama2005@gmail.com>
2021-11-02 11:06:43 -0400

    add final velocity answer display

--- static/scripts/kenimatics/v2_formula.js
+++ static/scripts/kenimatics/v2_formula.js
@@ -1,31 +1,58 @@
-// TIME
-// time given v1, v2, d
-function tGivenV1V2D(v1, v2, d){
-    return (2 * d) / (v2 + v1)
+// FINAL VELOCITY
+// final velocity given v1, a, d
+function v2GivenV1AD(v1, a, d){
+    return Math.sqrt(v1 ** 2 + 2 * a * d)
 }
 
-// time given v1, v2, a
-function tGivenV1V2D(v1, v2, a){
-    return (v2 - v1) / a
+// final velocity given t, d, a
+function v2GivenATD(a, t, d){
+    return ( a * t ** 2 + 2 * d) / (2 * t)
 }
 
-// function needs optomization
+// final velocity given v1, a, t
+function v2GivenATD(v1, a, t){
+    return a * t + v1
+}
+
+// final velocity given v1, a, t
+function v2GivenATD(v1, t, d){
+    return (-t * v1 + 2 * d) / (t)
+}
+
 document.getElementById('calculate-button').addEventListener('click', ()=>{
     // store all variables
     const v1 = document.getElementById('velocity1-input').value
-    const v2 = document.getElementById('velocity2-input').value
+    const t = document.getElementById('time-input').value
     const d = document.getElementById('displacement-input').value
     const a = document.getElementById('acceleration-input').value
 
     let answer
+    let image = document.getElementById('formula-img')
     
     // check which function to use
-    // given v1, v2, t
-    if (a === ''){
-        answer = dGivenV1V2D(v1, v2, d)
+    // given v1, t, a
+    if (v1 != '' && t != '' && a != ''){
+        answer = dGivenV1TA(v1, t, a) + 'm/s'
+        image.src = ''
     }
-    else if (d === ''){
-        answer = tGivenV1V2D(v1, v2, a)
+    // given v1, a, d
+    else if (v1 != '' && a != '' && d != ''){
+        answer = v2GivenV1AD(v1, a, d) + 'm/s'
+        image.src = ''
     }
-    
+    // given v1, a, t
+    else if (v1 != '' && a != '' && t != ''){
+        answer = v2GivenATD(v1, a, t) + 'm/s'
+        image.src = ''
+    }
+    // given v1, t, d
+    else if (v1 != '' && t != '' && d != ''){
+        answer = v2GivenATD(v1, t, d) + 'm/s'
+        image.src = ''
+    }
+    else {
+        answer = 'There was a problem with your input'
+        image.src = ''
+    }
+    document.getElementById('answer').innerText = 'ANSWER: ' + answer
 })

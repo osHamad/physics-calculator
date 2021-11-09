c2ede294cc8f79926cc097bd1d9f90895c6331ec
Osama Hamad <osamahama2005@gmail.com>
2021-11-09 11:51:00 -0500

    add a procfile

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express()
 const unitRouter = require('./routes/unit_selection')
 const kenimaticsRouter = require('./routes/kenimatics_selection')
 const dynamicsRoutes = require('./routes/dynamics_selection')
+const PORT = process.env.PORT || 5000
 
 app.set('view engine', 'ejs')
 app.use("/static", express.static('./static/'))
@@ -15,4 +16,4 @@ app.use('/units', unitRouter)
 app.use('/units/kenimatics', kenimaticsRouter)
 app.use('/units/dynamics', dynamicsRoutes)
 
-app.listen(8000)
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))

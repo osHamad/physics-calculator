b229b32251245eb17a6b10c97532fe52d37742b6
Osama Hamad <osamahama2005@gmail.com>
2021-11-09 18:47:09 -0500

    -

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const app = express()
 const unitRouter = require('./routes/unit_selection')
 const kenimaticsRouter = require('./routes/kenimatics_selection')
 const dynamicsRoutes = require('./routes/dynamics_selection')
-const PORT = process.env.PORT || 5000
 
 app.set('view engine', 'ejs')
 app.use("/static", express.static('./static/'))
@@ -15,5 +14,3 @@ app.get('/', (req, res)=>{
 app.use('/units', unitRouter)
 app.use('/units/kenimatics', kenimaticsRouter)
 app.use('/units/dynamics', dynamicsRoutes)
-
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`))

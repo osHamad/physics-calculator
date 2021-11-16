595eddd3cb02e64418485a9a1f67b1db74493184
Osama Hamad <osamahama2005@gmail.com>
2021-11-16 11:35:34 -0500

    add conversion rate display to length converter

--- static/scripts/conversion/length_convert.js
+++ static/scripts/conversion/length_convert.js
@@ -3,9 +3,21 @@ document.getElementById('calculate-button').addEventListener('click', ()=> {
     const lengthFrom = document.getElementById('conversion-units-1').value
     const lengthTo = document.getElementById('conversion-units-2').value
     const lengthValue = document.getElementById('length-input').value
-    const converted = rateToMeter[lengthTo.toLowerCase()][0] / rateToMeter[lengthFrom.toLowerCase()][0] * lengthValue
-    answer.innerText = 'CONVERTED: ' + converted.toString() + rateToMeter[lengthTo.toLowerCase()][1]
+    const conversionRate = rateToMeter[lengthTo.toLowerCase()][0] / rateToMeter[lengthFrom.toLowerCase()][0]
+    answer.innerText = 'CONVERTED: ' + conversionRate * lengthValue.toString() + rateToMeter[lengthTo.toLowerCase()][1]
+    setSide(conversionRate, lengthValue)
 })
+
+function setSide(con, l) {
+    const conversionRate = document.getElementById('conversion-rate')
+    const length = document.getElementById('length-given')
+    const rate = document.getElementById('rate-given')
+    const theDiv = document.getElementById('formula-div')
+    conversionRate.innerText=con
+    length.innerText=l
+    rate.innerText=con
+    theDiv.style.display = 'inline'
+}
 
 // conversion units from current unit to meter
 // map unit symbols to unit names for printing answer
